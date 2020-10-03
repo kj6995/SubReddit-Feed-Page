8cbc53106e786b5054730d2f424c734f2e9ff8b9
kj6995 <kushagra.me14iitp@gmail.com>
2020-10-04 00:37:30 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Article from './components/Article';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('Cricket');
+
+  useEffect( () => {
+    axios.get(`https://www.reddit.com/r/${subreddit}.json`)
+          .then(res => {
+            setArticles(res.data.data.children)
+          })
+          .catch(error => {
+            console.log(`faile due to ${error}`)
+          })
+  }, [subreddit])
+
   return (
     <div className="App">
+      <h1> SubReddit Feed App</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input  type="text" className="input" value={subreddit} placeholder="Type to search" onChange={e => setSubreddit(e.target.value)}/>
       </header>
+      <div className="articles">
+        {
+          (articles != null) ? articles.map((article,index) => <Article key={index} article={article.data} />) : ""
+        }
+      </div>
     </div>
   );
 }
